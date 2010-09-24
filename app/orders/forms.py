8dcd029ca3ce504d73a38fd0a684c6934602565d
Fredrik Nygård Carlsen <fredrik@fncit.no>
2010-09-25 01:50:45 +0200

    fikset forms for orders

--- app/orders/forms.py
+++ app/orders/forms.py
@@ -7,21 +7,15 @@ from django.contrib.auth.decorators import login_required, permission_required
 
 class OrderForm(ModelForm): 
     
-    project = forms.ModelChoiceField(Project.objects.for_company(), 
-                                     widget=SelectWithPop)
-    
-    
-    contacts = forms.ModelMultipleChoiceField(Contact.objects.for_company(), 
-                                              required=False, 
-                                              widget=MultipleSelectWithPop)
-     
-    participants = forms.ModelMultipleChoiceField(get_company_users(), 
-                                                  required=False, 
-                                                  widget=MultipleSelectWithPop) 
-    
-    
-    def __init__(self,*args,**kwargs):
-        super (OrderForm,self ).__init__(*args,**kwargs) # populates the post
+
+
+    def __init__(self,*args,**kwrds):
+        super(OrderForm,self).__init__(*args,**kwrds)
+        self.fields['project'].widget = SelectWithPop()
+        self.fields['project'].queryset=Project.objects.for_company()
+        self.fields['contacts'].widget = MultipleSelectWithPop()
+        self.fields['contacts'].queryset=Contact.objects.for_company()
+        
         self.fields['responsible'].queryset = get_company_users()
         
     class Meta:
@@ -30,9 +24,7 @@ class OrderForm(ModelForm):
 
 
 class OrderFormSimple(ModelForm):
-    project         = forms.ModelChoiceField(Project.objects.for_company(), widget=SelectWithPop)
-    responsible     = forms.ModelChoiceField(get_company_users())
-
+    
     class Meta:
         model = Order
         exclude = ('deleted', 'date_created', 'date_edited', 'owner','creator','editor','company','contacts',
