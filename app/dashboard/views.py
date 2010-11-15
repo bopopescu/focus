0f8f2e56129dd60c2bc2c95709109231bc5295da
Fredrik Nygård Carlsen <fredrik@fncit.no>
2010-11-15 11:00:52 +0100

    fikset sånn at all log vises i oversikt, var noe feil med filteret

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -10,24 +10,20 @@ from core.middleware import get_current_company, get_current_user
 
 @login_required
 def overview(request):
-    
-    
     updateTimeout(request)
     announcements = Announcement.objects.for_user()[::-1]
     your_projects = Project.objects.for_user()
     your_orders = Order.objects.for_user()[::-1]
-        
+
     return render_with_request(request, 'dashboard/dashboard.html', {'title':'Oppslagstavle',
                                                                      'announcements':announcements,
                                                                      'orders':your_orders,
                                                                      'projects':your_projects})
 
 def logs(request):
-
     logs = Log.objects.filter(company=get_current_company())
-    return render_with_request(request, 'dashboard/listLog.html', {'title':'Siste 30 hendelser',
-                                                                   'logs': logs[1:30][::-1]} )
-
+    return render_with_request(request, 'dashboard/listLog.html', {'title': 'Siste 30 hendelser',
+                                                                   'logs': logs[::-1][0:30]})
 
 def notifications(request):
 
@@ -43,7 +39,7 @@ def notifications(request):
     #Set to read, so they wont bother the user anymore.
     Notification.objects.filter(recipient=get_current_user()).update(read=True)
 
-
     return render_with_request(request, 'dashboard/notifications.html', {'title':'Siste 30 hendelser',
                                                                          'notifications': newNotifications,
-                                                                         'oldNotifications': oldNotifications} )
+                                                                         'oldNotifications': oldNotifications[::-1][
+                                                                                             0:30]})
