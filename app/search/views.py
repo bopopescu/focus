6486ff04a4a86831383a055efc4561b64975c659
Fredrik Nygård Carlsen <fredrik@fncit.no>
2010-11-19 00:17:17 +0100

    fjernet case-sensitive søk, lagt til søk på oppslag

--- app/search/views.py
+++ app/search/views.py
@@ -1,4 +1,3 @@
-
 from app.customers.models import Customer
 from app.projects.models import Project
 from django.http import HttpResponse
@@ -8,38 +7,39 @@ from app.orders.models import Order
 from app.suppliers.models import Supplier
 from app.stock.models import Product
 from django.contrib.auth.decorators import login_required
+from app.announcements.models import Announcement
 
 @login_required
 def search(request):
-
     term = request.GET.get('s')
 
     searchIn = {}
 
-    searchIn[Customer]  = ["full_name",'email','address','phone']
-    searchIn[Project]   = ["project_name",'pid','responsible__first_name']
-    searchIn[Contact]   = ["full_name","email",'address','phone']
-    searchIn[Order]     = ["order_name","customer__full_name",'project__project_name','description']
-    searchIn[Supplier]  = ["name"]
-    searchIn[Product]   = ['name', 'description','productGroup__name']
+    searchIn[Customer] = ["full_name", 'email', 'address', 'phone']
+    searchIn[Project] = ["project_name", 'pid', 'responsible__first_name']
+    searchIn[Contact] = ["full_name", "email", 'address', 'phone']
+    searchIn[Order] = ["order_name", "customer__full_name", 'project__project_name', 'description']
+    searchIn[Supplier] = ["name"]
+    searchIn[Product] = ['name', 'description', 'productGroup__name']
+    searchIn[Announcement] = ["title", "text"]
 
     result = {}
 
     for o in searchIn.keys():
         for i in searchIn[o]:
-            kwargs = {'%s__%s' % ('%s'%i, 'contains'): '%s'%term}
+            kwargs = {'%s__%s' % ('%s' % i, 'icontains'): '%s' % term}
             k = o.objects.for_user().filter(**kwargs)
 
             for s in k:
-               if s in result:
-                   result[s]+=1
-               else:
-                   result[s]=1
+                if s in result:
+                    result[s] += 1
+                else:
+                    result[s] = 1
 
     result = sorted(result.items(), lambda x, y: cmp(x[1], y[1]), reverse=True)
     v = []
     for i in result:
         v.append(i[0])
-    
+
     return render_with_request(request, 'search/list.html', {'title':'Resultat',
-                                                                'objects':v})
+                                                             'objects':v})
