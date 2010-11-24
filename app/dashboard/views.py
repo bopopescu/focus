4d78ab28ccbec6ba9802e698f94e88ab8e362e9d
Fredrik Nygård Carlsen <fredrik@fncit.no>
2010-11-24 12:44:44 +0100

    fikset slik at det kun vises aktive ordrer i oversikten på forsiden

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -13,7 +13,7 @@ def overview(request):
     updateTimeout(request)
     announcements = Announcement.objects.for_user()[::-1]
     your_projects = Project.objects.for_user()
-    your_orders = Order.objects.for_user()[::-1]
+    your_orders = Order.objects.for_user().filter(state="O")[::-1]
 
     return render_with_request(request, 'dashboard/dashboard.html', {'title':'Oppslagstavle',
                                                                      'announcements':announcements,
