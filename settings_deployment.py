2979a147bf19eb2263d10c7070df712b491fcaba
frecar <fredrik@fncit.no>
2011-01-28 18:13:09 +0100

    string fix

--- settings_deployment.py
+++ settings_deployment.py
@@ -6,7 +6,8 @@ TEMPLATE_DEBUG = DEBUG
 import os
 
 f = open(os.getenv('HOME')+'/passwords/db-focus.txt', 'rb')
-DB_PASSWORD = f.readline()
+DB_PASSWORD = f.readline().strip()
+f.close()
 
 DATABASES = {
     'default': {
