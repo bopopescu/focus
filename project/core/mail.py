aee0412d7c78e64a9a98bf2d955b0fd0575b9f3d
Fredrik Carlsen <fredrik@fncit.no>
2011-03-13 17:07:33 +0100

    Fisket litt slik at debug-email alltid blir sendt, men faktisk epost kun når debug=False

--- project/core/mail.py
+++ project/core/mail.py
@@ -4,12 +4,14 @@ from django.conf import settings
 
 def send_mail(subject, message, fromMail, toMails, fail_silently=False):
 
-    recipients = [settings.DEBUG_EMAIL]
+    recipients = []
+    recipients.extend(toMails)
 
-    if settings.DEBUG:
-        subject = "[debug] subject: %s" % subject
-        message = "from: %s, to: %s\n\n%s" % (fromMail, recipients, message)
-        mail = django_send_mail(subject, message, fromMail, recipients, fail_silently=fail_silently)
-    else:
-        recipients.extend(toMails)
+    #Always send debug email
+    subject = "[debug] subject: %s" % subject
+    message = "from: %s, to: %s\n\n%s" % (fromMail, recipients, message)
+    mail = django_send_mail(subject, message, fromMail, [settings.DEBUG_EMAIL], fail_silently=fail_silently)
+
+    #Send actual email if not in debug mode
+    if not settings.DEBUG:
         mail = django_send_mail(subject, message, fromMail, recipients, fail_silently=fail_silently)
