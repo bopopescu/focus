d2d681bd35083c722ff5b3540bf0417a457bf2aa
frecar <fredrik@fncit.no>
2011-03-16 05:38:12 +0100

    skiftet db-engine til postgres

--- project/settings_deployment.py
+++ project/settings_deployment.py
@@ -12,7 +12,7 @@ f.close()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.psycopg2',
         'NAME': 'frecarlsen_focus',
         'USER': 'frecarlsen',
         'PASSWORD': DB_PASSWORD,
