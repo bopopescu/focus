43774d3b72ebcb3d72ff4f2529eaa763368dc74a
frecar <fredrik@fncit.no>
2011-03-17 01:01:48 +0100

    fjernet tomme epostadresser fra utsending

--- project/core/mail.py
+++ project/core/mail.py
@@ -12,6 +12,13 @@ def send_mail(subject, message, fromMail, toMails, fail_silently=False):
     message = "from: %s, to: %s\n\n%s" % (fromMail, recipients, message)
     mail = django_send_mail(subject, message, fromMail, [settings.DEBUG_EMAIL], fail_silently=fail_silently)
 
+    print recipients
+    #Clean recipient list for non-emails
+    for email in recipients:
+        if email == "":
+            recipients.remove(email)
+
+    print recipients
     #Send actual email if not in debug mode
     if not settings.DEBUG and len(recipients)>0:
         mail = django_send_mail(subject, message, fromMail, recipients, fail_silently=fail_silently)
