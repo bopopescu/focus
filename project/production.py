4276e6ff6c2c9f0213cf932f2094c37ba1d37455
frecar <fredrik@fncit.no>
2011-04-13 02:08:21 +0200

    fikset så jeg får sendt mail på prod-dev

--- project/production.py
+++ project/production.py
@@ -1,6 +1,6 @@
 from settings import *
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 import os
@@ -19,3 +19,13 @@ DATABASES = {
 }
 
 STATIC_URL = '/static/'
+
+
+#EMAIL DEBUG
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'focustimeno@gmail.com'
+EMAIL_HOST_PASSWORD = '4th56y44g'
+EMAIL_PORT = 587
+
+DEBUG_EMAIL = "fredrik@fncit.no"
