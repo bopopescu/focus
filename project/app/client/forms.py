e6782cc3112a994e1b50849f2b29aa652b61c75f
jpmossin <thefreeskier@gmail.com>
2011-05-23 00:45:00 +0200

    Some client ticket fixes (create)

--- project/app/client/forms.py
+++ project/app/client/forms.py
@@ -6,7 +6,7 @@ from django.utils.translation import ugettext as _
 
 class ClientTicketForm(ModelForm):
     """
-    Form form making comments
+    Form for making comments
     """
     comment = forms.CharField(widget=forms.Textarea, label=_("Add Comment"), required=False)
     attachment = forms.FileField(label=_("Add Attachment"), required=False)
@@ -39,12 +39,14 @@ class ClientNewTicketForm(ModelForm):
         super(ModelForm, self).__init__(*args, **kwargs)
         self.client = client
         self.fields['company'].queryset = client.get_related_companys()
+        self.fields['company'].empty_label = None
 
     def save(self, commit=True):
         ticket = super(ClientNewTicketForm, self).save(commit=False)
         ticket.client_user = self.client
-        ticket.type = TicketType.objects.all()[0]
-        #TicketType.objects.get_or_create(name=_("Client ticket"), description=_("Submitted by a client"))[0]
+        ticket.type = TicketType.objects.get_or_create(name=_("Client ticket"),
+                                                       company=self.cleaned_data['company'],
+                                                       description=_("Submitted by a client"))[0]
         ticket.save(commit=commit)
         self.client.tickets.add(ticket)
 
