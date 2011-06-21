8a5d334da45be8f899802bda5bbbea881fe5236e
jpmossin <thefreeskier@gmail.com>
2011-06-21 15:00:04 +0200

    hourregs. now created from timer when stopped

--- project/api/hourregistrationsapi/models.py
+++ project/api/hourregistrationsapi/models.py
@@ -1,7 +1,9 @@
+from decimal import Decimal
+from app.hourregistrations.models import HourRegistration
 from core.managers import PersistentManager
 from core.models import PersistentModel
 from django.db import models
-from datetime import datetime
+from datetime import datetime, date
 
 
 class TimeTrackerManager(models.Manager):
@@ -38,28 +40,35 @@ class TimeTracker(PersistentModel):
         if current and not current.done:
             current.stop_and_save()
 
-    def stop_and_save(self):
+    def stop_and_save(self, description, order):
         """
         stops the timer and creates an Hourregistration
         """
         self.pause()
         self.active = False
         self.save()
-        time_info = self.time_info
-        self.create_hour_reg()
+        self.create_hour_reg(description, order)
 
+
+    def create_hour_reg(self, description, order):
+        time_info = self.time_info()
+        sec_worked, sec_paused = time_info[:2]
+        start_time, end_time = time_info[2:]
+
+        hours_worked = Decimal(sec_worked) / Decimal(3600)
+        hours_paused = Decimal(sec_paused) / Decimal(3600)
+
+        time_start = u'%d:%d' % (start_time.hour, start_time.minute)
+        time_end = u'%d:%d' % (end_time.hour, end_time.minute)
+        HourRegistration.objects.create(date=date.today(), order=order, description=description,
+                                            hours_worked=hours_worked, pause=hours_paused,
+                                            time_start=time_start, time_end=time_end)
 
     def is_running(self):
         current = self.current_period()
         if current and not current.done:
             return True
         return False
-
-
-    def create_hour_reg(self):
-        periods = WorkPeriod.objects.filter(tracker=self).order_by("id")
-
-        # TODO: create HourRegistration from this TimeTracker
 
 
     def current_period(self):
@@ -70,24 +79,29 @@ class TimeTracker(PersistentModel):
 
     def time_info(self):
         """
-        returns (seconds_worked, seconds_paused)
+        returns (seconds_worked, seconds_paused, start_time, end_time)
+        start_time and end_time are datetime objects
         """
         periods = WorkPeriod.objects.filter(tracker=self).order_by("id")
         if not periods:
-            return 0,0
+            return 0,0,-1,-1
         total_worked = periods[0].as_seconds()
+        time_start = periods[0].start
         total_paused = 0
         for i in range(1, len(periods)):
             total_worked += periods[i].as_seconds()
             diff = periods[i].start - periods[i-1].end
             total_paused += ((diff.days * 24 * 3600) + diff.seconds)
 
+        last = periods[len(periods)-1]
         if self.active:
-            last = periods[len(periods)-1]
+            time_end = -1
             if last.done:
                 pause = (datetime.now() - last.end)
                 total_paused += ((pause.days *24 * 3600) + pause.seconds)
-        return total_worked, total_paused
+        else:
+            time_end = last.end
+        return total_worked, total_paused, time_start, time_end
 
 
 
