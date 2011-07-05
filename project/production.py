0cd601749b42749610fe67a62f94c92782b586eb
Ubuntu <ubuntu@srv2.fncit.no>
2011-07-05 13:07:06 +0000

    fjernet unødvendige instillinger

--- project/production.py
+++ project/production.py
@@ -31,12 +31,4 @@ CACHES = {
     }
 }
 
-#EMAIL DEBUG
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'focustimeno@gmail.com'
-EMAIL_HOST_PASSWORD = '4th56y44g'
-EMAIL_PORT = 587
-SERVER_EMAIL = "fredrik@fncit.no"
-
 DEBUG_EMAIL = "fredrik@fncit.no"
