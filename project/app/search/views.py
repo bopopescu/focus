6a7ab00531b26da47a4d0d4dea215b7dd7f90703
Fredrik Nygård Carlsen <fredrik@fncit.no>
2011-10-22 18:18:44 +0200

    cachet søk pr company

--- project/app/search/views.py
+++ project/app/search/views.py
@@ -1,7 +1,9 @@
 from app.customers.models import Customer
 from app.projects.models import Project
 from app.contacts.models import Contact
+from django.core.cache import cache
 from app.tickets.models import Ticket, TicketUpdate
+from core import Core
 from core.decorators import login_required
 from django.shortcuts import render
 from app.orders.models import Order
@@ -13,12 +15,18 @@ from app.announcements.models import Announcement
 def search(request):
     term = request.GET.get('s')
 
+    cache_key = "%s_%s" % (Core.current_user().get_company().id, term)
+
+    if cache.get(cache_key):
+        return render(request, 'search/list.html', {'title': 'Resultat',
+                                                    'objects': cache.get(cache_key)})
+
     searchIn = {}
 
     searchIn[Customer] = ["name", 'email', 'address', 'phone']
     searchIn[Project] = ["project_name", 'pid', 'responsible__first_name']
     searchIn[Contact] = ["name", "email", 'address', 'phone']
-    searchIn[Ticket] = ["title","description"]
+    searchIn[Ticket] = ["title", "description"]
     searchIn[TicketUpdate] = ["comment"]
     searchIn[Order] = ["title", "customer__name", 'project__project_name', 'description']
     searchIn[Supplier] = ["name"]
@@ -28,7 +36,7 @@ def search(request):
     result = {}
 
     terms = term.split(" ")
-    
+
     for o in searchIn.keys():
         for i in searchIn[o]:
             kwargs = {'%s__%s' % ('%s' % i, 'icontains'): '%s' % term}
@@ -39,7 +47,7 @@ def search(request):
                     result[s] += 1
                 else:
                     result[s] = 1
-            
+
             for t in terms:
                 kwargs = {'%s__%s' % ('%s' % i, 'icontains'): '%s' % t}
                 k = o.objects.filter_current_company().filter(**kwargs)
@@ -56,5 +64,7 @@ def search(request):
     for i in result:
         v.append(i[0])
 
+    cache.set(cache_key, v)
+
     return render(request, 'search/list.html', {'title': 'Resultat',
-                                                             'objects': v})
+                                                'objects': v})
